814c6f18a4759cafb45723b1d0d9fbbb29b2d5a2
hworoshch <hworoshch@gmail.com>
2019-05-22 20:55:21 +0700

    lesson 3

---
+++ db/migrate/20190522134931_create_answers.rb
@@ -0,0 +1,12 @@
+class CreateAnswers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :answers do |t|
+      t.text :body, :null => false
+      t.boolean :correct, :null => false, :default => false
+      t.integer :question_id, :null => false
+      t.integer :user_id, :null => false
+
+      t.timestamps
+    end
+  end
+end
