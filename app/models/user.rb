813debe1a17346e1f379d5b060e21ddaedde4272
hworoshch <hworoshch@gmail.com>
2019-05-23 18:31:55 +0700

    4th lesson

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+  def tests_by_level(level)
+    answers = Answer.where('user_id = ?', self.id)
+    questions = Question.find(answers.pluck(:question_id))
+    tests = Test.find(questions.pluck(:test_id)).select { |t| t.level == level }
+  end
+
 end
