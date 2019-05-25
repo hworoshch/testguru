794707bb45ed3f43c5c5f568ffe4d026babc8136
hworoshch <hworoshch@gmail.com>
2019-05-25 19:50:38 +0700

    validations

--- app/models/test.rb
+++ app/models/test.rb
@@ -9,4 +9,11 @@ class Test < ApplicationRecord
   scope :medium, -> { level(2..4) }
   scope :hard, -> { level(5..Float::INFINITY) }
   scope :by_category, -> (category_title) { joins(:category).where(categories: { title: category_title }) }
+
+  #validates :title, presence: true
+  #validates :level, presence: true
+  validates :level, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true,
+                    uniqueness: { scope: :level, case_sensitive: false }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
 
+  validates :name, presence: true
+
   def by_level(test_level)
     tests.level(test_level)
   end
