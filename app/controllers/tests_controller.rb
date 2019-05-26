84831216650aee2f425ef1c139ee769527079703
hworoshch <hworoshch@gmail.com>
2019-05-27 02:47:36 +0700

    bugfixes

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,16 +1,8 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show]
-
   def index
     @tests = Test.all
     respond_to do |format|
       format.html 
     end
-  end
-
-  private
-
-  def find_test
-    @test = Test.find(params[:id])
   end
 end
