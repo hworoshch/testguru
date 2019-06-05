ef58d3cc7572ff4598ce86a1f96701ad902343c5
hworoshch <hworoshch@gmail.com>
2019-06-06 01:24:48 +0700

    authentication

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,7 @@
 class TestsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_test, only: [:show, :edit, :update, :destroy, :start]
-  before_action :set_user #, only: :start
+  before_action :set_user, only: :start
 
   def index
     @tests = Test.all
@@ -50,7 +51,7 @@ class TestsController < ApplicationController
   end
 
   def set_user
-    @user = User.first
+    @user = current_user
   end
 
   def test_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
 
   validates :name, presence: true
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
