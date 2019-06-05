a3c182b19c342eae44b6a31816b9354bfe420494
hworoshch <hworoshch@gmail.com>
2019-06-05 12:37:21 +0700

    asdf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_084853) do
+ActiveRecord::Schema.define(version: 2019_06_04_115345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2019_05_28_084853) do
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
     t.integer "question_id", null: false
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
