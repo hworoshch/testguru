23b38b57706fa240445efd9578ef62835cfcdbae
hworoshch <hworoshch@gmail.com>
2019-06-12 14:13:59 +0700

    11th

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,10 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
-  def authenticate_user!
-    unless current_user
-      session[:referer] = request.url
-      redirect_to login_path, alert: 'Please login'
-    end
-    cookies[:email] = current_user&.email
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
+  def configure_permitted_parameters
+    attributes = [:name, :lastname]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
   root to: 'tests#index'
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  get :logout, to: 'sessions#destroy'
-  resources :users, only: :create
-  resources :sessions, only: :create
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
     end
@@ -17,5 +13,9 @@ Rails.application.routes.draw do
     member do
       get :result
     end
+  end
+
+  namespace :admin do
+    resources :tests
   end
 end
