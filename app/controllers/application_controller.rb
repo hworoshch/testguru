17c7ae6143e93734ce54f5e3e3281df90528f6ed
hworoshch <hworoshch@gmail.com>
2019-06-14 19:51:48 +0700

    Tests localized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   protected
 
@@ -15,5 +20,11 @@ class ApplicationController < ActionController::Base
     elsif resource.class == User
       root_path
     end
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
